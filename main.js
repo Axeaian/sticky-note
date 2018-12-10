cb8d454833094e2baddef05a11f7f32a192dafbb
Axeaian <xf.chia@outlook.com>
2018-12-10 08:46:57 +0800

    can add and save

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ db = [
 ];
 
 let state = "save";
+let stateIndicator = document.getElementById("state");
 let editB = document.getElementById("edit");
+let addB = document.getElementById("add");
 let saveB = document.getElementById("save");
 
 //load sticky
@@ -33,32 +35,67 @@ function load(note) {
 db.forEach(function(sticky) {
   load(sticky);
 });
+stateIndicator.innerText = state;
 
 //to edit sticky
 function edit() {
   if (state === "save" || state === "edit") {
     state = "edit";
+    stateIndicator.innerText = state;
     editB.innerText = "Editing... ";
     let pArray = document.querySelectorAll("p");
     pArray.forEach(function(item) {
       item.setAttribute("contenteditable", "true");
     });
+  } else {
+    alert("Please save to continue");
+  }
+}
+
+//to add sticky
+function add() {
+  if (state === "save" || state === "add") {
+    if (db.length > 0 && db[0].title === "" && db[0].content === "") {
+      alert("Please use empty sticky first");
+    } else {
+      state = "add";
+      stateIndicator.innerText = state;
+      load({ title: "", content: "" });
+      db.unshift({ title: "", content: "" });
+      pArray = document.querySelectorAll("p");
+      pArray[0].setAttribute("contenteditable", "true");
+      pArray[0].addEventListener("input", function() {
+        db[0].title = this.innerText;
+      });
+      pArray[1].setAttribute("contenteditable", "true");
+      pArray[1].addEventListener("input", function() {
+        db[0].content = this.innerText;
+      });
+    }
+  } else {
+    alert("Please save to continue");
   }
 }
 
 //to save sticky
 function save() {
-  if (state === "edit") {
+  if (state === "edit" || state === "add") {
     let pArray = document.querySelectorAll("p");
     pArray.forEach(function(item) {
       item.setAttribute("contenteditable", "false");
     });
-    editB.innerText = "save";
+    editB.innerText = "Edit";
+    state = "save";
+    stateIndicator.innerText = state;
   }
 }
 
 editB.addEventListener("click", function() {
   edit();
+});
+
+addB.addEventListener("click", function() {
+  add();
 });
 saveB.addEventListener("click", function() {
   save();
