d51c2c966663fb7a7261a5f5ccabd04596d770cb
Axeaian <xf.chia@outlook.com>
2018-12-10 08:25:54 +0800

    can edit and save

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ db = [
   { title: "Second Sticky", content: "Let's see how it looks" }
 ];
 
+let state = "save";
+let editB = document.getElementById("edit");
+let saveB = document.getElementById("save");
+
 //load sticky
 function load(note) {
   let node = document.createElement("div");
@@ -10,12 +14,14 @@ function load(note) {
 
   let title = document.createElement("p");
   title.setAttribute("class", "title");
+  title.setAttribute("contenteditable", "false");
   let tit = document.createTextNode(note.title);
   title.appendChild(tit);
   node.appendChild(title);
 
   let content = document.createElement("p");
   content.setAttribute("class", "content");
+  content.setAttribute("contenteditable", "false");
   let cont = document.createTextNode(note.content);
   content.appendChild(cont);
   node.appendChild(content);
@@ -26,4 +32,34 @@ function load(note) {
 // load sticky from db
 db.forEach(function(sticky) {
   load(sticky);
+});
+
+//to edit sticky
+function edit() {
+  if (state === "save" || state === "edit") {
+    state = "edit";
+    editB.innerText = "Editing... ";
+    let pArray = document.querySelectorAll("p");
+    pArray.forEach(function(item) {
+      item.setAttribute("contenteditable", "true");
+    });
+  }
+}
+
+//to save sticky
+function save() {
+  if (state === "edit") {
+    let pArray = document.querySelectorAll("p");
+    pArray.forEach(function(item) {
+      item.setAttribute("contenteditable", "false");
+    });
+    editB.innerText = "save";
+  }
+}
+
+editB.addEventListener("click", function() {
+  edit();
+});
+saveB.addEventListener("click", function() {
+  save();
 });
